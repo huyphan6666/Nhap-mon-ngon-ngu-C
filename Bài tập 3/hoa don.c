6fc382058ee25b035dba01f8b223a05228659fbf
huyphan677777 <72533787+huyphan6666@users.noreply.github.com>
2020-12-16 17:38:08 -0800

    hoa don.c
    int main(void) {
    int dongia, soluong, thanhtien;
      printf("nhap don gia va so luong\n");
    scanf("%d%d", &dongia, &soluong); 
    thanhtien = dongia * soluong;
    if (soluong >= 5 && dongia >= 500){
      printf("so tien can phai thanh toan la %d", thanhtien * 50/100);
    }else{
      printf("so tien can phai thanh toan la %d", thanhtien);
    }
      return 0;
    }

--- Bài tập 3/hoa don.c
+++ Bài tập 3/hoa don.c
@@ -1,1 +1,12 @@
-
+int main(void) {
+int dongia, soluong, thanhtien;
+  printf("nhap don gia va so luong\n");
+scanf("%d%d", &dongia, &soluong); 
+thanhtien = dongia * soluong;
+if (soluong >= 5 && dongia >= 500){
+  printf("so tien can phai thanh toan la %d", thanhtien * 50/100);
+}else{
+  printf("so tien can phai thanh toan la %d", thanhtien);
+}
+  return 0;
+}
