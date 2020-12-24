c82786d873a4d8d66e40aef41c0247bc601a96c3
huyphan677777 <72533787+huyphan6666@users.noreply.github.com>
2020-12-23 20:03:26 -0800

    tong 10.c
     #include <stdio.h>
     
     int main (vold){
     int dem, tong;
     tong = 0;
       for( dem = 1; dem <= 10; dem ++){
           tong = tong + dem;
    } 
    printf("tong la : %d, tong ");
    return 0;
    }

---
+++ tong 10.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+ 
+ int main (vold){
+ int dem, tong;
+ tong = 0;
+   for( dem = 1; dem <= 10; dem ++){
+       tong = tong + dem;
+} 
+printf("tong la : %d, tong ");
+return 0;
+}
