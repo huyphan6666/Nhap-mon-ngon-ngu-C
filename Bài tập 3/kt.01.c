73bbc053ed60865d2d81cdd558c1c7130b3e9422
huyphan677777 <72533787+huyphan6666@users.noreply.github.com>
2021-01-07 10:37:51 +0700

    kt_01.c
    #include <stdio.h>
    int main (){
    int a, b, c;
    a = 2; b = 3; c = 4;
    printf ("BANG CUU CHUONG 2\n ");
    printf("%d * 1 = %d/n ", a, a * 1 );
    printf("%d * 2 = %d/n" , a, a * 2 );
    printf("%d * 3 = %D/n" , a, a * 3 );
    printf ("BANG CUU CHUONG 3\n");
    printf("%d * 1= %d/n ", b, b * 1 );
    printf("%d * 2= %d/n ", b, b * 2 ); 
    printf("%d * 3= %d/n ", b, b * 3 );
    printf ("BANG CUU CHUONG 4\n");
    printf("%d * 1 = %d/n ", c, c * 1 );
    printf("%d * 2 = %d/n ", c, c * 2 );
    printf("%d * 3 = %d/n ", c, c * 3 );
    return 0;
    }

--- Bài tập 3/kt.01.c
+++ Bài tập 3/kt.01.c
@@ -4,8 +4,8 @@ int main(){
     printf("\n-------CHUONG TRINH IN BANG CUU CHUONG-------\n");
         // in ra màn hinh dòng b?ng c?u chuong  
     for (int i = 2; i <= 10; i++){
-    	// in ra vòng l?p t? 2 d?n 9  
-        printf("\n--------BANG CUU CHUONG %D--------\n", i);
+    	// in ra vòng lap tu 2 den 9  
+        printf("\n--------BANG CUU CHUONG %d--------\n", i);
         for(int j = 2; j < 10; j++){
             printf("\t%d x %d = %d\n", i, j, i*j);
         }
