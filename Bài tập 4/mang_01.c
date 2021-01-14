72948e68fc4c35d4782dbb6b55ea2813c59d2f3e
huyphan677777 <72533787+huyphan6666@users.noreply.github.com>
2021-01-14 09:54:31 +0700

    mang_01.c
    #include <stdio.h>
    
    int main () {
    	int mang[4] = {3, 5, 7, 2};
    	int tong = 0;
    	for (int i = 0; i < 4; i++) {
    		tong += mang[i];
    	}
    	printf (" tong la: %d", tong);
    	return 0;
    }

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
 int main () {
-  int mang[4];
-  //mang nay bat dau bang so 4   
-  printf("nhap 4 so tu ban phim: ");
-  //hien thi nhap 4 so ra man hinh  
-  for (int i = 0; i < 4; i++) 
-  	{
-  // vòng lap bat dau tu 0 và bé hon 4 theo mang [4]  
-    scanf("%d", &mang[i]);
-    //nhap so cua mang a tu ban phim  
-  }
-  for (int i = 0; i < 4; i++) {
-  // Lap lai vong lap cua i  
-    printf("%d ", mang[i]);
-    //xuat ra cac gia tri co trong mang [4] ra man hinh  
-  }
-  return  0;
+	int mang[4] = {3, 5, 7, 2};
+	int tong = 0;
+	for (int i = 0; i < 4; i++) {
+		tong += mang[i];
+	}
+	printf ("%d", tong);
+	return 0;
 }
-
