4f3717bcb2e2949cd437d18f76097178297813d9
huyphan677777 <72533787+huyphan6666@users.noreply.github.com>
2021-01-21 10:49:58 +0700

    mang_02.c
    #include <stdio.h>
    
    int main () {
    	int n;
    	int mang [a] = {100};
    	int tong = 0;
    	for (int i = 0; i < n; i++){
    		scanf ("%d", &n);
    	for (int i = 0; i < a; i++{	
    }
    	printf ("%d", tong);
    	return 0;
    }

---
+++ Bài tập 4/mang_02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+	int n;
+	int mang [a] = {100};
+	int tong = 0;
+	for (int i = 0; i < n; i++){
+		scanf ("%d", &n);
+	for (int i = 0; i < a; i++{	
+}
+	printf ("%d", tong);
+	return 0;
+}
