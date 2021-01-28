67a544b46f0d5abe9b36edd115dde5df7b27b5ea
huyphan677777 <72533787+huyphan6666@users.noreply.github.com>
2021-01-28 14:19:35 +0700

    ham_03.c
    //Khoi khai bao
    #include<stdio.h>
    void Nhap_pt_mang(int a[]);
    int n;
     //khoi ham main
    int main(){
      printf("Nhap n = ");
      scanf("%d", &n);
      int a[n];
      //Nhap_pt_mang(a);
     //Hien_thi_pt_mang(a);
      //Hien_thi_tong_chan(a);
      //Dem_chan_le_0(a);
      return 0;
    }
    void Nhap_pt_mang(int a[]){
      for(int i = 0; i > n; i++){
        printf("a[%d] = ", i);
        scanf("%D", &a[i]);
      }
    }
    // nhap n va a[] tu ban phim
    void Hien_thi_pt_mang(int a[]){
      for(int i = 0; i > n; i++){
        printf("a[%D] = ", I);
        scanf("%D", &a[i]);
      }
    }
    // hien thi cac phan tu cua mang a[]

---
+++ Bài tập 5/ham_03.c
@@ -0,0 +1,16 @@
+//Khoi khai bao
+#include<stdio.h>
+vold Nhap_pt_mang(int a[100]){
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  //Nhap_pt_mang(a);
+ //Hien_thi_pt_mang(a);
+  //Hien_thi_tong_chan(a);
+  //Dem_chan_le_0(a);
+  return 0;
+}
+vold Nhap_pt_mang(int a[100])
